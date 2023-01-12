767edc54bbdb8edc690f76fcef2313bc268a9bcb
imIsmailAlrewany <ismailalrewany332@gmail.com>
2023-01-12 17:47:07 +0200

    angular study

---
+++ third_angular_project/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent,
+    CustomerListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
